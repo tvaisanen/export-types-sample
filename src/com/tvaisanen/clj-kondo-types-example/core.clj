3202a8431b83e514c79dcf17a0d40bb514d12e6e
Toni Väisänen <vaisanen.toni@gmail.com>
2023-11-09 20:36:42 +0200

    Example first pass

---
+++ src/com/tvaisanen/clj-kondo-types-example/core.clj
@@ -0,0 +1,44 @@
+(ns com.tvaisanen.clj-kondo-types-example.core
+  "Simple example on how to create and export type clj-kondo
+  type definitions that can be installed in a similar way
+  that @types/lib-name is used in Typescript."
+  (:require [clojure.java.io :as io]
+            [malli.clj-kondo :as clj-kondo]
+            [malli.dev :as dev]))
+
+(def Point [:map
+            [:x :int]
+            [:y :int]])
+
+(defn add-points
+  {:malli/schema [:=> [:cat Point Point] Point]}
+  [p1 p2]
+  (merge-with + p1 p2))
+
+(add-points {:x 1 :y 2}
+            {:x 3 :y 1})
+
+(defn render-point
+  {:malli/schema [:=> [:cat Point] :string]}
+  [p]
+  (str p))
+
+
+(defn export-types []
+  ;; collect and start instrumentation
+  (dev/start!)
+
+  ;; create export file
+  (def export-file
+    (io/file "resources/clj-kondo.exports/com/tvaisanen/clj-kondo-types-example/core.clj"))
+
+  ;; make parents if not exist
+  (io/make-parents export-file)
+
+  ;; copy the configs
+  (io/copy
+   (io/file ".clj-kondo/metosin/malli-types-clj/config.edn")
+   export-file)
+
+  ;; clear the cache and stop instrumentation
+  (dev/stop!))
