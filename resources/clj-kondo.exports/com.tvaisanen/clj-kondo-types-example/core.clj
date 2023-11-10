429b4f19ef3e3dd68ab770646c7272252b4c5e78
Toni Väisänen <vaisanen.toni@gmail.com>
2023-11-10 21:07:29 +0200

    Remove old ns from config

--- resources/clj-kondo.exports/com.tvaisanen/clj-kondo-types-example/core.clj
+++ resources/clj-kondo.exports/com.tvaisanen/clj-kondo-types-example/core.clj
@@ -1,23 +1,19 @@
-{:linters {:unresolved-symbol {:exclude [(malli.core/=>)]},
-           :type-mismatch {:namespaces {com.tvaisanen.core {add-points {:arities {2 {:args [{:op :keys,
-                                                                                             :req {:x :int, :y :int}}
-                                                                                            {:op :keys,
-                                                                                             :req {:x :int, :y :int}}],
-                                                                                     :ret {:op :keys,
-                                                                                           :req {:x :int, :y :int}}}}},
-                                                            render-point {:arities {1 {:args [{:op :keys,
-                                                                                               :req {:x :int, :y :int}}],
-                                                                                       :ret :string}}}},
-                                        com.tvaisanen.clj-kondo-types-example.core {render-point {:arities {1 {:args [{:op :keys,
-                                                                                                                       :req {:x :int,
-                                                                                                                             :y :int}}],
-                                                                                                               :ret :string}}},
-                                                                                    add-points {:arities {2 {:args [{:op :keys,
-                                                                                                                     :req {:x :int,
-                                                                                                                           :y :int}}
-                                                                                                                    {:op :keys,
-                                                                                                                     :req {:x :int,
-                                                                                                                           :y :int}}],
-                                                                                                             :ret {:op :keys,
-                                                                                                                   :req {:x :int,
-                                                                                                                         :y :int}}}}}}}}}}
+{:linters
+ {:unresolved-symbol {:exclude [(malli.core/=>)]},
+  :type-mismatch
+  {:namespaces
+   {com.tvaisanen.clj-kondo-types-example.core
+
+    {render-point {:arities {1 {:args [{:op  :keys,
+                                        :req {:x :int,
+                                              :y :int}}],
+                                :ret  :string}}},
+     add-points   {:arities {2 {:args [{:op  :keys,
+                                        :req {:x :int,
+                                              :y :int}}
+                                       {:op  :keys,
+                                        :req {:x :int,
+                                              :y :int}}],
+                                :ret  {:op  :keys,
+                                       :req {:x :int,
+                                             :y :int}}}}}}}}}}
